cd298930fbca95bf66f949b5120d0dc215507314
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-03-03 21:37:19 -0300

    criando as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css';
+import AppRoutes from './AppRoutes';
 
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <AppRoutes />
     </div>
   );
 }
